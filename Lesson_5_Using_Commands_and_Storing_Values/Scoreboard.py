39e81b7f7f54e79ca154a2482e234cf12e4af28a
isbees <isbeesbrukhman@gmail.com>
2022-10-20 14:34:44 -0400

    first commit

---
+++ Lesson_5_Using_Commands_and_Storing_Values/Scoreboard.py
@@ -0,0 +1,41 @@
+''' 
+Today we will learn about:
+- Review what's a string 
+- Dictionaries and accessing a peice of the element
+- adding variables to commands 
+- looping with input
+- += operator
+'''
+
+# What is a string?
+#   Answer: characters that are inbetween "" or ''
+#           A string can have any length 
+
+students1 = {
+    "Feranmi": 5,
+    "Sam": 1,
+    "Lorrenzo": 1,
+    "Dvimedh": 2
+    }
+
+students2 = {
+    "Arya": 8,
+    "Yuvan": 6,
+    "Ryan": 5
+    }
+
+# this will just find the student and add the points
+def add_points(student, points):
+    students1.get(student) += int(points)
+    # we can also use students1[student] += int(points)
+
+
+def display_scores():
+    print(students1)
+
+while True:
+    display_scores()
+
+    student_name = input("Enter the good student's name: ")
+    student_points = input("Enter the points earned: ")
+    add_points(student_name, student_points)
